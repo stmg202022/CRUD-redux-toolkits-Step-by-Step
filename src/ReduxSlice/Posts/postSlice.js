e9e9e818052752ea5bc757d7cbc347012a6b08b5
Samson Tmg <stmg202023@gmail.com>
2023-08-21 15:15:30 +0545

    postSlice created and add in a store

---
+++ src/ReduxSlice/Posts/postSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [
+    {
+      id: 1,
+      title: "Learn HTML",
+      content: "Learn HTML...",
+    },
+    {
+      id: 2,
+      title: "Learn CSS",
+      content: "Learn CSS...",
+    },
+    {
+      id: 3,
+      title: "Learn JS",
+      content: "Learn js...",
+    },
+  ],
+};
+const PostSlice = createSlice({
+  name: "Post",
+  initialState,
+  reducers: {},
+});
+
+export default PostSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -3,8 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 //counterSlice
 import CounterSlice from "./ReduxSlice/Counter/counterSlice";
 
+//postSlice
+import PostSlice from "./ReduxSlice/Posts/postSlice";
+
 export const store = configureStore({
   reducer: {
     count: CounterSlice,
+    posts: PostSlice,
   },
 });
